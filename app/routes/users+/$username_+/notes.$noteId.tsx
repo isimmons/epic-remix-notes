3c29369c279d5ab3a55ac3027177aebc2534abb7
isimmons <isimmons33@gmail.com>
2024-06-21 21:58:47 -0400

    fix scrollbar covering text

--- app/routes/users+/$username_+/notes.$noteId.tsx
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -25,7 +25,7 @@ export default function NoteId() {
   return (
     <div className="absolute inset-0 flex flex-col px-10">
       <h2 className="mb-2 pt-12 text-h2 lg:mb-6">{note.title}</h2>
-      <div className="overflow-y-auto pb-24">
+      <div className="overflow-y-auto px-3 pb-24">
         <p className="whitespace-break-spaces text-sm md:text-lg">
           {note.content}
         </p>
